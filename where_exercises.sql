935b377986aa4f80e151feb47013af7d29fca226
bichtran <tranngocb@gmail.com>
2017-06-01 16:00:39 -0500

    Commit SQL where_exercices:  Select and use WHERE (Chained) to filter data on employees table.

--- where_exercises.sql
+++ where_exercises.sql
@@ -16,5 +16,28 @@ SELECT emp_no, first_name, last_name, birth_date FROM employees WHERE birth_date
 SELECT 'Employees with a ''q'' in their last name — 1,873 rows.';
 SELECT emp_no, first_name, last_name, birth_date FROM employees WHERE last_name LIKE '%q%';
 
+SELECT 'WHERE Chained';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE first_name ='Irena' OR first_name='Vidya' OR first_name='Maya';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE (first_name ='Irena' OR first_name='Vidya' OR first_name='Maya') AND gender = 'M';
+
+SELECT 'Find all employees whose last name starts or ends with ''E'' — 30,723 rows.';
 
 
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+select'Find all employees hired in the 90s and born on Christmas — 362 rows.' as '90 and Xmas';
+
+SELECT emp_no, first_name, last_name
+FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25';
+
+SELECT 'Find all employees with a ''q'' in their last name but not ''qu'' — 547 rows.'
+SELECT emp_no, first_name, last_name
+FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
